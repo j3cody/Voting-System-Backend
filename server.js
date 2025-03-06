e6356ed0a8f759f29b9e52f53403e6c3137883b3
j3cody <jatinpatidar040@gmail.com>
2025-03-07 02:07:02 +0530

    mongodb added

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Connect to MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/voting", {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
